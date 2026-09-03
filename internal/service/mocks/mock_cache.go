09f7d9182cc9963d3905fdd553c8654eb4927f33
Derbik-Git <daniilderbenev@yandex.ru>
2026-09-03 16:34:04 +0300

    fix code

--- internal/service/mocks/mock_cache.go
+++ internal/service/mocks/mock_cache.go
@@ -2,7 +2,6 @@ package mocks
 
 import (
 	"context"
-	"errors"
 	"time"
 
 	"github.com/Derbik-Git/user-service/internal/domain"
@@ -16,7 +15,7 @@ type CacheMock struct {
 
 func (c *CacheMock) GetUser(ctx context.Context, id int64) (*domain.User, error) {
 	if c.GetUserFunc == nil {
-		return nil, errors.New("cache.GetUser was not expected to be called in this test") // допиши текст ошибок
+		return nil, nil
 	}
 
 	return c.GetUserFunc(ctx, id)
@@ -24,7 +23,8 @@ func (c *CacheMock) GetUser(ctx context.Context, id int64) (*domain.User, error)
 
 func (c *CacheMock) SetUser(ctx context.Context, u *domain.User, ttl time.Duration) error {
 	if c.SetUserFunc == nil {
-		return errors.New("cahce.SetUser is not implemented for this test case")
+		return nil
+
 	}
 
 	return c.SetUserFunc(ctx, u, ttl)
@@ -32,8 +32,24 @@ func (c *CacheMock) SetUser(ctx context.Context, u *domain.User, ttl time.Durati
 
 func (c *CacheMock) DeleteUser(ctx context.Context, id int64) error {
 	if c.DeleteUserFunc == nil {
-		return errors.New("cahce.DeleteUser is not implementedfor this test case")
+		return nil
 	}
 
 	return c.DeleteUserFunc(ctx, id)
 }
+
+/*
+Если вам нужно жестко контролировать вызовы, то в каждом тест-кейсе в service_unit_test.go вы обязаны передавать функции реализации:
+
+func TestService_UpdateUser(t *testing.T) {
+    cacheMock := &mocks.CacheMock{
+        SetUserFunc: func(ctx context.Context, u *domain.User, ttl time.Duration) error {
+            // Передаем nil, если хотим, чтобы запись в кэш прошла успешно
+            return nil
+        },
+    }
+
+    svc := service.NewUserService(repoMock, cacheMock, brokerMock, nil, time.Minute)
+    // ... дальнейший вызов svc.UpdateUser(...)
+}
+*/
